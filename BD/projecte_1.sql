6700a0217ddae9713bf06c7e24e922436f878e48
jAcebo <35202.joan23@fje.edu>
2016-10-17 17:10:23 +0200

    Creación de carpeta

---
+++ BD/projecte_1.sql
@@ -0,0 +1,67 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Servidor: 127.0.0.1
+-- Temps de generació: 16-10-2016 a les 23:41:20
+-- Versió del servidor: 10.1.16-MariaDB
+-- Versió de PHP: 5.6.24
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de dades: `projecte_1`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de la taula `anunci`
+--
+
+CREATE TABLE `anunci` (
+  `anu_id` int(4) NOT NULL,
+  `anu_titol` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  `anu_data_anunci` date NOT NULL,
+  `anu_data_robatori` date NOT NULL,
+  `anu_ubicacio_robatori` varchar(35) COLLATE utf8_spanish_ci NOT NULL,
+  `anu_descripcio_robatori` varchar(200) COLLATE utf8_spanish_ci NOT NULL,
+  `anu_marca` varchar(15) COLLATE utf8_spanish_ci NOT NULL,
+  `anu_model` varchar(25) COLLATE utf8_spanish_ci NOT NULL,
+  `anu_color` varchar(25) COLLATE utf8_spanish_ci NOT NULL,
+  `anu_antiguitat` varchar(25) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `anu_descripcio` text COLLATE utf8_spanish_ci NOT NULL,
+  `anu_numero_serie` varchar(15) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `anu_foto` varchar(25) COLLATE utf8_spanish_ci NOT NULL,
+  `anu_compensacio` decimal(4,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Indexos per taules bolcades
+--
+
+--
+-- Index de la taula `anunci`
+--
+ALTER TABLE `anunci`
+  ADD PRIMARY KEY (`anu_id`);
+
+--
+-- AUTO_INCREMENT per les taules bolcades
+--
+
+--
+-- AUTO_INCREMENT per la taula `anunci`
+--
+ALTER TABLE `anunci`
+  MODIFY `anu_id` int(4) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
